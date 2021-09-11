3eb5d77985d57419208987c05f2cd643ecef88eb
Kevin Jerome <kjerome1997@gmail.com>
2021-09-10 20:45:09 -0700

    Add problems 026-029

---
+++ data-structures-and-algorithms/medium/026-three-number-sum.py
@@ -0,0 +1,33 @@
+"""
+Given an input array of integers, return an array of all triplets which
+sum up to a target sum.
+
+The triplets should be sorted in ascending order.
+"""
+
+def threeNumberSum(array, targetSum):
+	"""
+	Time: O(n^2) - iterate through main array and for each number, we iterate through the rest of the array.
+	Space: O(n)
+	"""
+
+	array.sort()
+	three_number_sums = []
+
+	for idx in range(len(array)):
+		left_ptr = idx + 1
+		right_ptr = len(array) - 1
+
+		while left_ptr < right_ptr:
+			current_sum = array[idx] + array[left_ptr] + array[right_ptr]
+
+			if current_sum == targetSum:
+				three_number_sums.append([array[idx], array[left_ptr], array[right_ptr]])
+				left_ptr += 1
+				right_ptr -= 1
+			elif current_sum < targetSum:
+				left_ptr += 1
+			else:
+				right_ptr -= 1
+
+	return three_number_sums
---
+++ data-structures-and-algorithms/medium/029-monotonic-array.py
@@ -0,0 +1,19 @@
+"""
+Return true if the array is either monotonically increasing or monotonically decreasing.
+"""
+
+def isMonotonic(array):
+	"""
+	Time: O(n) - we iterate through the array once
+	Space: O(1) - we don't allocate any input-dependent memory
+	"""
+	isNonDecreasing = True
+	isNonIncreasing = True
+
+	for index in range(1, len(array)):
+		if array[index] > array[index - 1]:
+			isNonIncreasing = False
+		if array[index] < array[index - 1]:
+			isNonDecreasing = False
+
+	return isNonDecreasing or isNonIncreasing
