fb028485b84b506f00882cbcf30a26e52306012d
Kevin Jerome <kjerome1997@gmail.com>
2021-09-13 19:11:02 -0700

    Add third solution to 002-two-number-sum.py

--- data-structures-and-algorithms/questions/easy/002-two-number-sum.py
+++ data-structures-and-algorithms/questions/easy/002-two-number-sum.py
@@ -32,3 +32,29 @@ def twoNumberSum(array, targetSum):
         if y != x and y in stored_numbers:
             return [x, targetSum - x]
     return []
+
+
+def twoNumberSum(array, targetSum):
+    """
+    Pointer approach
+    O(nlogn) time and O(1) space
+    """
+    left_pos = 0
+    right_pos = len(array) - 1
+
+    array.sort()
+
+    while left_pos < right_pos:
+        left_val = array[left_pos]
+        right_val = array[right_pos]
+
+        current_sum = left_val + right_val
+
+        if current_sum == targetSum:
+            return [left_val, right_val]
+        elif current_sum > targetSum:
+            right_pos -= 1
+        else:
+            left_pos += 1
+
+    return []
